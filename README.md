b17ac98f2fdbb6605e66b15e87c1860b57b27495
mohammadRaiyyan <inforaiyyan@gmail.com>
2024-11-26 19:05:46 +0530

    Updated readme

--- README.md
+++ README.md
@@ -1,3 +1,96 @@
 # React Modal Library
 
 This React library uses stack based approach which reduces the unwanted z index handling.
+
+![Logo](./public/img.png)
+
+### Install
+```bash
+npm i @raiyyan/react-overlay-modal
+```
+
+### Wrap the root app with `ModalProvider`
+
+```typescript jsx
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.tsx"
+import { ModalProvider } from "@raiyyan/react-overlay-modal"
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ModalProvider>
+      <App />
+    </ModalProvider>
+  </StrictMode>,
+)
+```
+
+```typescript jsx
+
+import { useModal } from "@raiyyan/react-overlay-modal"
+
+function ModalContent() {
+  const { closeModal } = useModal()
+  return (
+    <div className="test-modal">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt fugit
+      impedit molestias blanditiis amet numquam ipsa maxime unde, dolor quo,
+      iste inventore officia accusamus delectus voluptate, nulla illum earum
+      debitis consectetur? Nobis corrupti quisquam libero blanditiis nemo rem
+      consequuntur omnis quod velit autem, obcaecati inventore dolorem? Sunt
+      tempora totam dolor.
+      <br />
+      <div className="action-container">
+        <button className="btn secondary" onClick={closeModal}>
+          Close
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default function Page() {
+  const { showModal } = useModal()
+  return (
+        <button
+          className="btn primary"
+          onClick={() => {
+            showModal({
+              content: <ModalContent />,
+              config: { size: "2xl"},
+            })
+          }}
+        >
+          Trigger Modal
+        </button>
+  )
+}
+```
+### API
+
+#### Modal Size
+| Value                     | Description                                          |
+|---------------------------|------------------------------------------------------|
+| "sm"                      | Small modal size.                                   |
+| "md" (default)            | Medium modal size.                                  |
+| "lg"                      | Large modal size.                                   |
+| "xl"                      | Extra-large modal size.                             |
+| "2xl"                     | Double extra-large modal size.                      |
+| "3xl"                     | Triple extra-large modal size.                      |
+| "full"                    | Fullscreen modal.                                   |
+| { width: string; height: string } | Custom modal size with specified width and height. |
+
+#### Config
+
+| Property              | Type                             | Default Value   | Description                                           |
+|-----------------------|----------------------------------|-----------------|-------------------------------------------------------|
+| size                  | ModalSize                       | "md"            | Specifies the size of the modal (see `ModalSize`).    |
+| disableOutsideClick   | boolean                         | false           | If true, disables clicking outside the modal to close it. |
+
+####  showModalProps
+
+| Property | Type                   | Description                                  |
+|----------|------------------------|----------------------------------------------|
+| config   | ModalConfig            | Configuration for the modal. Defaults: `size: "md"`, `disableOutsideClick: false`. |
+| content  | ReactElement           | The content to display inside the modal.    |
--- src/Page.tsx
+++ src/Page.tsx
@@ -1,8 +1,7 @@
 import { useModal } from "../"
 
-function ModalNested3() {
-  const { showModal, closeModal } = useModal()
-
+function ModalContent() {
+  const { closeModal } = useModal()
   return (
     <div className="test-modal">
       praesentium ullam nesciunt tempore laborum animi ducimus? Omnis ad quo
@@ -21,89 +20,8 @@ function ModalNested3() {
       tempora totam dolor.
       <br />
       <div className="action-container">
-        <button
-          className="btn primary"
-          onClick={() =>
-            showModal({
-              content: (
-                <div className="test-modal">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Pariatur accusantium nemo cum possimus repudiandae odio
-                  distinctio reiciendis voluptas maiores officiis!
-                </div>
-              ),
-              config: { size: "md" },
-            })
-          }
-        >
-          Opene new
-        </button>
         <button className="btn secondary" onClick={closeModal}>
           Close
-        </button>
-      </div>
-    </div>
-  )
-}
-function ModalNested2() {
-  const { showModal, closeModal } = useModal()
-
-  return (
-    <div className="test-modal">
-      praesentium ullam nesciunt tempore laborum animi ducimus? Omnis ad quo
-      expedita reiciendis possimus libero fugiat maxime sint voluptatem ratione
-      a sapiente nulla odio dolor voluptas corrupti, earum debitis beatae
-      perspiciatis recusandae. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Temporibus, repellat reiciendis exercitationem cum
-      laudantium corporis necessitatibus asperiores sunt? Neque, soluta.
-      <br />
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat quam
-      cupiditate est, labore porro commodi totam et explicabo necessitatibus
-      autem praesentium officiis enim dolorem deserunt at saepe eveniet ab illo
-      error. Vitae in doloribus quidem nostrum voluptate asperiores laboriosam
-      omnis ducimus distinctio odio, amet fuga non molestias, architecto earum
-      maiores?
-      <div className="action-container">
-        <button className="btn secondary" onClick={closeModal}>
-          Close
-        </button>
-        <button
-          className="btn primary"
-          onClick={() =>
-            showModal({ content: <ModalNested3 />, config: { size: "md" } })
-          }
-        >
-          Opene new
-        </button>
-      </div>
-    </div>
-  )
-}
-function ModalContent() {
-  const { showModal, closeModal } = useModal()
-  return (
-    <div className="test-modal">
-      <div className="grid">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => (
-          <div className="card">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio
-            doloremque libero eveniet aspernatur, sequi impedit obcaecati ad id
-            odit, veniam autem soluta iste corrupti. Harum voluptate asperiores
-            sed pariatur facere.
-          </div>
-        ))}
-      </div>
-      <div className="action-container">
-        <button className="btn secondary" onClick={closeModal}>
-          Close
-        </button>
-        <button
-          className="btn primary"
-          onClick={() =>
-            showModal({ content: <ModalNested2 />, config: { size: "xl" } })
-          }
-        >
-          Opene new
         </button>
       </div>
     </div>
@@ -120,7 +38,7 @@ export default function Page() {
           onClick={() => {
             showModal({
               content: <ModalContent />,
-              config: { size: "2xl", disableOutsideClick: true },
+              config: { size: "2xl"},
             })
           }}
         >
